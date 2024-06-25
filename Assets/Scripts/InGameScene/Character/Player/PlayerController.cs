bc1c2f60ec6e6f62dc06527f1455d0acffee8469
MSI\Izumi Yuuhi <yuuhi809@gmail.com>
2024-06-25 16:45:14 +0900

    Playerの移動の調整

--- Assets/Scripts/InGameScene/Character/Player/PlayerController.cs
+++ Assets/Scripts/InGameScene/Character/Player/PlayerController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 3f; // 最大移動速度
+    public float speed = 50f; // 最大移動速度
     public float acceleration = 2f; // 加速度
-    public float tiltAmount = 15f; // 傾きの角度
-    public float tiltSpeed = 10f; // 傾く速度
+    public float tiltAmount = 0.5f; // 傾きの角度
+    public float tiltSpeed = 5f; // 傾く速度
 
-    private Vector3 velocity = Vector3.zero; // 現在の速度
+    private Vector3 velocity; // 現在の速度
     private float targetTilt = 0f; // 目標の傾き
 
     void Start()
@@ -25,19 +25,19 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            verticalInput += 1f;
+            verticalInput += speed;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            verticalInput -= 1f;
+            verticalInput -= speed;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            horizontalInput -= 1f;
+            horizontalInput -= speed;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            horizontalInput += 1f;
+            horizontalInput += speed;
         }
 
         // 入力方向のベクトル
@@ -54,10 +54,25 @@ public class PlayerController : MonoBehaviour
 
         // プレイヤーの位置を画面内に制限
         Vector3 newPosition = transform.position;
-        newPosition.x = Mathf.Clamp(newPosition.x, -3.84f, 3.49f); // 画面の左右端の座標に制限
-        newPosition.y = Mathf.Clamp(newPosition.y, -0.8f, 1.66f); // 画面の上下端の座標に制限
+        newPosition.x = Mathf.Clamp(newPosition.x, -5.61f, 5.62f); // 画面の左右端の座標に制限
+        newPosition.y = Mathf.Clamp(newPosition.y, -0.5954228f, 3.0f); // 画面の上下端の座標に制限
         transform.position = newPosition;
-
+        if (newPosition.x >= 5.62f) 
+        {
+            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * acceleration);
+        }
+        if (newPosition.x <= -5.61f) 
+        {
+            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * acceleration);
+        }
+        if (newPosition.y <= -0.5954228f) 
+        {
+            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * acceleration);
+        }
+        if (newPosition.y >= 3.0f) 
+        {
+            velocity = Vector3.Lerp(velocity, Vector3.zero, Time.deltaTime * acceleration);
+        }
         // 水平入力に基づいてZ軸を傾ける
         targetTilt = -horizontalInput * tiltAmount;
         float tilt = Mathf.LerpAngle(transform.localEulerAngles.z, targetTilt, Time.deltaTime * tiltSpeed);
